852bd9fe7f6b481c26feaac9b908be5244787416
Annie-Peng <djs910532@gmail.com>
2024-01-17 14:15:31 +0800

    modify: update FormGroupInput style

--- src/common/components/FormGroupInput.jsx
+++ src/common/components/FormGroupInput.jsx
@@ -1,26 +1,35 @@
-import { Fragment } from "react";
-
-const FormGroupInput = ({ groupDataSet, register, error}) => {
+const FormGroupInput = ({ groupDataSet, register, errors}) => {
 
   if(!groupDataSet) return;
 
   return (
-      <div>
+    <div className={`flex flex-col`}>
         {groupDataSet.map((data, index)=>{
-            const { inputClass, labelClass, hClass, errClass, type, accept, hMsg, name, placeholder, validation } = data;
+            const { inputClass, labelClass, hClass, errClass, type, accept, hMsg, name, placeholder, validation, required, id, key } = data;
+
+            const requiredClass = required !== false && "after:content-['*'] after:ml-0.5 after:text-red-500";
+
           return (
-            <Fragment key={index}>
-              <label className={labelClass} htmlFor={name}>
-                <h3 className={hClass}>{hMsg}</h3>
-              </label>
-              <input className={inputClass} id={name} name={name} type={type} accept={accept} placeholder={placeholder} {...register(name, validation)}/>
-              {error && <p className={errClass}>{error.message}</p>}
-            </Fragment>
+            <>
+              {index === 0 && <hr className="my-2 w-[95%] mx-auto"/>}
+              <div key={index} className="flex items-center relative">
+                <div className="flex items-center w-full gap-2 p-2">
+                  <label className={`w-1/4 text-right ${labelClass}`} htmlFor={name}>
+                    <h3 className={`resumeH3 ${hClass} ${requiredClass}`}>{hMsg}</h3>
+                  </label>
+                  <input className={inputClass} id={name} name={name} type={type} accept={accept} placeholder={placeholder} {...register(name, validation)}/>
+                  {errors && <p className={`text-sm text-red-500 ${errClass}`}>{errors?.[id]?.[key]?.message}</p>}
+                </div>
+                {index === 0 && (
+                  <div className="flex gap-2 absolute right-[20px]">
+                    <button className="addBtn btn" type="button">+</button>
+                    <button className="deleteBtn btn" type="button" >-</button>
+                  </div>
+                )}
+              </div>
+            </>
           )
         })}
-        <button type="button" >+ 增加</button>
-        <button type="button" >- 刪除</button>
-        <hr />
       </div>
     )
 }
