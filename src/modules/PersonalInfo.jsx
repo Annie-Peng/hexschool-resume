9506d8aee17a5f7f0d6151df1c79b7c377cb6b7a
Annie-Peng <djs910532@gmail.com>
2024-01-17 14:17:54 +0800

    feat: connect PersonalInfo with FormGroupInput

--- src/modules/PersonalInfo.jsx
+++ src/modules/PersonalInfo.jsx
@@ -4,13 +4,16 @@ import { controller } from "../dataSet/controller";
 import { FormContext } from "../common/features/FormContext";
 import { resumeDataSet } from '../dataSet/resumeDataSet';
 import { personalInfoTestData } from '../dataSet/testDataSet';
+import FormGroupInput from "../common/components/FormGroupInput";
+import { useState } from "react";
 
 
 const PersonalInfo = () => {
 
   const { title, formDataSet } = resumeDataSet.personalInfo;
+  const [ renderItem, setRenderItem ] = useState(formDataSet);
   const { personalInfo, updateForm } = useContext(FormContext);
-  const { register, handleSubmit, formState: {errors} } = useForm({
+  const { register, handleSubmit, formState: {errors}, unregister } = useForm({
     defaultValues: personalInfoTestData
   });
 
@@ -26,7 +29,10 @@ const PersonalInfo = () => {
       <h2 className="resumeH2">{title}</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         {formDataSet.map((formData, index)=>{
-          const RenderForm = controller[formData.component];
+          const RenderForm = controller[formData.component]; // 選擇表單元件
+          if(formData.group) { // group表單
+            return <FormGroupInput initGroupDataSet={formDataSet[index]} groupDataSet={formData?.group} register={register} errors={errors[formData.group[0].group]} key={index} renderItem={renderItem} setRenderItem={setRenderItem} formIndex={index} unregister={unregister}/>
+          }
           return (
             <Fragment key={index}>
               {RenderForm && <RenderForm formData={formData} register={register} error={errors[formData.name]}/>}
