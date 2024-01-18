fd5a031e4a085e49d853a91e259b22617811960c
Annie-Peng <djs910532@gmail.com>
2024-01-18 14:43:48 +0800

    feat: add Declaration style

--- src/modules/Declaration.jsx
+++ src/modules/Declaration.jsx
@@ -3,25 +3,35 @@ import { useForm } from 'react-hook-form';
 import { controller } from "../dataSet/controller";
 import { FormContext } from "../common/features/FormContext";
 import { resumeDataSet } from '../dataSet/resumeDataSet';
+import { declarationTestDataSet } from "../dataSet/testDataSet";
 
 
 const Declaration = () => {
 
   const { title, formDataSet } = resumeDataSet.declaration;
   const { declaration } = useContext(FormContext);
-  const { register, handleSubmit, formState: {errors} } = useForm();
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    defaultValues: declarationTestDataSet
+  });
 
   const onSubmit = (values) => {
     console.log(values);
   }
 
   // console.log(errors);
-  console.log(declaration);
 
   return (
-    <section>
-      <h2>{title}</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <section className="resumeSection">
+      <h2 className="resumeH2">{title}</h2>
+      <ul>
+        <li>
+          1、本人同意多角人才基於就業服務之目的依法蒐集、處理、利用個人資料於任用相關作業之進行。
+        </li>
+        <li>
+          2、本人確認所提供之個人資料及相關經歷均屬實，如有不實情形，願意承擔相應責任。 
+        </li>
+      </ul>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-wrap items-center justify-between">
         {formDataSet.map((formData, index)=>{
           const RenderForm = controller[formData.component];
           return (
@@ -30,6 +40,7 @@ const Declaration = () => {
             </Fragment>
           )
         })}
+        <p className="w-[120px]">日期 {declaration.signatureUpdatedTime}</p>
       </form>
     </section>
   );
