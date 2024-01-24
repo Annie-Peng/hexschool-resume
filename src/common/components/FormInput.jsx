ec879480531e22fbf470a5622ae566ca0ab50c61
Annie-Peng <djs910532@gmail.com>
2024-01-24 10:36:08 +0800

    feat: add watch to FormInput

--- src/common/components/FormInput.jsx
+++ src/common/components/FormInput.jsx
@@ -1,8 +1,11 @@
+import { useEffect } from "react";
 import { requiredClass } from "../../dataSet/validationMsg";
+import { useState } from "react";
 
-const FormInput = ({formData, register, error, formClass}) => {
+const FormInput = ({formData, register, error, formClass, watch}) => {
 
   const { type, accept, hMsg, name, placeholder, validation, pMsg, required, disabled } = formData;
+  const [ newDisabled, setNewDisabled ] = useState(disabled);
 
   const { inputClass,
     errClass,
@@ -11,13 +14,20 @@ const FormInput = ({formData, register, error, formClass}) => {
     pClass,
     labelClass } = formClass;
 
+    useEffect(() => {
+      const { name, group, id, key } = formData;
+      if(name.includes("endYear") || name.includes("endMonth")){
+        const watchItem = watch(`${group}.${id}.${key}.isLeft`);
+        setNewDisabled(watchItem > 0 && true);
+      }
+    }, [watch]);
+
   return (
-    // <></>
     <div className={`flex items-center p-2 gap-2 ${outerClass}`}>
       <label className={`w-1/4 text-right ${labelClass}`} htmlFor={name}>
         <h3 className={`resumeH3 ${hClass} ${requiredClass(required)}`}>{hMsg}</h3>
       </label>
-      <input className={`${error && "focus:outline-red-500"} ${inputClass}`} id={name} type={type} accept={accept} placeholder={placeholder} disabled={disabled} {...register(name, validation)} />
+      <input className={`${error?.message && "focus:outline-red-500 border-red-500"} ${inputClass}`} id={name} type={type} accept={accept} placeholder={placeholder} disabled={newDisabled} {...register(name, validation)} />
       {pMsg && <p className={pClass}>{pMsg}</p>}
       {error && <p className={`resumeErr ${errClass}`}>{error.message}</p>}
     </div>
