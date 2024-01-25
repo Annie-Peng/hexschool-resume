312fd94f1193a25f341f55500e2d0162e6575b69
Annie-Peng <djs910532@gmail.com>
2024-01-25 16:00:25 +0800

    modify: update useCusForm with FormProvider

--- src/common/hook/useCusForm.jsx
+++ src/common/hook/useCusForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { FormContext } from '../features/FormContext';
 import { resumeDataSet } from '../../dataSet/resumeDataSet';
 import { useEffect } from 'react';
@@ -11,7 +11,9 @@ const useCusForm = ({defaultValues, formTitle}) => {
   const { updateForm } = useContext(FormContext);
   const [ edit, setEdit  ] = useState(false);
 
-  const { handleSubmit, reset, ...formFunctions } = useForm({defaultValues});
+  const formFunctions = useForm({defaultValues});
+  
+  const { handleSubmit, reset } = formFunctions;
 
   const onSubmit = (values) => {
     updateForm({name: formTitle, values})
@@ -23,18 +25,20 @@ const useCusForm = ({defaultValues, formTitle}) => {
   }, []);
 
   const Form = ({ children }) => (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {children}
-      {edit && (
-        <div className="flex justify-center gap-4 mt-4">
-        <button className="cancelledBtn btn" type="button" onClick={()=>{
-          setEdit(false);
-          reset(defaultValues);
-        }}>取消</button>
-        <button className="saveBtn btn" type="submit">儲存</button>
-      </div>
-      )}
-    </form>
+    <FormProvider {...formFunctions}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {children}
+        {edit && (
+          <div className="flex justify-center gap-4 mt-4">
+          <button className="cancelledBtn btn" type="button" onClick={()=>{
+            setEdit(false);
+            reset(defaultValues);
+          }}>取消</button>
+          <button className="saveBtn btn" type="submit">儲存</button>
+        </div>
+        )}
+      </form>
+    </FormProvider>
   );
 
   return {handleSubmit, formFunctions, Form, renderItem, setRenderItem, formDataSet, title, edit, setEdit}
