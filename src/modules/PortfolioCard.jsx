b850e5a0766a911f0a71672218594b84c793aef3
Annie-Peng <djs910532@gmail.com>
2024-01-28 21:12:17 +0800

    feat: add PortfolioCard module

---
+++ src/modules/PortfolioCard.jsx
@@ -0,0 +1,29 @@
+import { useFormContext } from "react-hook-form"
+import Input from "../common/components/refactor/Input"
+
+const PortfolioCard = ({formDataSet, data, dataName, error, edit}) => {
+
+  const { getValues } = useFormContext();
+
+  return (
+    <>
+    {edit ? (
+      <div className="ml-auto w-[96%] border-2 border-secondary-500 rounded-md">
+      <div className="relative flex flex-col py-6">
+        {Object.keys(data).map((subKey, index) => (
+          <Input key={index} formDataSet={formDataSet} dataName={`portfolio.items.${subKey}`} name={`${dataName}.${subKey}`} error={error} edit={edit}/>
+        ))}
+      </div>
+    </div>
+    ) : (
+      <div className="flex flex-col gap-2">
+      {Object.keys(data).map((subKey, index) => (
+          <p key={index}>{getValues(`${dataName}.${subKey}`)}</p>
+        ))}
+      </div>
+    ) }
+    </>
+  )
+}
+
+export default PortfolioCard
