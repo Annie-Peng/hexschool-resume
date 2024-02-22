3adb9ef7101d5c726fc99ec36875a848bb38d6b7
Annie-Peng <djs910532@gmail.com>
2024-02-22 15:52:12 +0800

    modify: update Form buttons

--- src/common/components/Form.jsx
+++ src/common/components/Form.jsx
@@ -1,19 +1,15 @@
 import { FormProvider } from "react-hook-form";
 
-const Form = ({ children, formFunctions, edit, setEdit, onSubmit }) => {
-  const { handleSubmit, reset } = formFunctions;
+const Form = ({ children, formFunctions, edit, onSubmit }) => {
+  const { handleSubmit } = formFunctions;
 
   return (
     <FormProvider {...formFunctions}>
       <form onSubmit={handleSubmit(onSubmit)}>
         {children}
         {edit && (
-          <div className="flex justify-center gap-4 mt-4">
-            <button className="cancelledBtn btn" type="button" onClick={() => {
-              setEdit(false);
-              reset();
-            }}>取消</button>
-            <button className="saveBtn btn" type="submit">儲存</button>
+          <div className="flex justify-end gap-4 mt-4">
+            <button className="saveBtn btn" type="submit">更新</button>
           </div>
         )}
       </form>
