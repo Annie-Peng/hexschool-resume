b59fc37cefc6a17177e324f8ca1471027009d767
Annie-Peng <djs910532@gmail.com>
2024-02-23 13:55:30 +0800

    modify: update btns style

--- src/common/components/Form.jsx
+++ src/common/components/Form.jsx
@@ -3,7 +3,7 @@ import { FormProvider } from "react-hook-form";
 const Form = ({ children, formFunctions, edit, onSubmit }) => {
   const { handleSubmit, formState: { dirtyFields } } = formFunctions;
 
-  const switchBtnClass = Object.keys(dirtyFields).length === 0 ? "cancelledBtn" : "saveBtn";
+  const switchBtnClass = Object.keys(dirtyFields).length === 0 ? "borderPrimaryBtn" : "saveBtn";
 
   return (
     <FormProvider {...formFunctions}>
@@ -11,7 +11,7 @@ const Form = ({ children, formFunctions, edit, onSubmit }) => {
         {children}
         {edit && (
           <div className="flex justify-end gap-4 mt-4">
-            <button className={`btn ${switchBtnClass}`} type="submit">更新</button>
+            <button className={`btn !w-[120px] ${switchBtnClass}`} type="submit">更新</button>
           </div>
         )}
       </form>
