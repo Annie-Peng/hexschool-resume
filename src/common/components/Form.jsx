2249d6af755a417a43810831debcde88462685f8
Annie-Peng <djs910532@gmail.com>
2024-02-23 10:30:06 +0800

    feat: add switchBtnClass to Form when input is dirty

--- src/common/components/Form.jsx
+++ src/common/components/Form.jsx
@@ -1,7 +1,9 @@
 import { FormProvider } from "react-hook-form";
 
 const Form = ({ children, formFunctions, edit, onSubmit }) => {
-  const { handleSubmit } = formFunctions;
+  const { handleSubmit, formState: { dirtyFields } } = formFunctions;
+
+  const switchBtnClass = Object.keys(dirtyFields).length === 0 ? "cancelledBtn" : "saveBtn";
 
   return (
     <FormProvider {...formFunctions}>
@@ -9,7 +11,7 @@ const Form = ({ children, formFunctions, edit, onSubmit }) => {
         {children}
         {edit && (
           <div className="flex justify-end gap-4 mt-4">
-            <button className="saveBtn btn" type="submit">更新</button>
+            <button className={`btn ${switchBtnClass}`} type="submit">更新</button>
           </div>
         )}
       </form>
