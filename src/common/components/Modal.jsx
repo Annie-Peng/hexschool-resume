ae1c45677ac42c5bbd6e6c3c4c728534c76b3012
Annie-Peng <djs910532@gmail.com>
2024-02-29 13:42:30 +0800

    modify: update Modal class, pass children(close)

--- src/common/components/Modal.jsx
+++ src/common/components/Modal.jsx
@@ -1,11 +1,11 @@
 import Popup from "reactjs-popup";
 
-const Modal = ({ btnText, children }) => {
+const Modal = ({ btnText, children, modalClass }) => {
   return (
     <Popup
           modal
           trigger={
-            <button className="!w-fit btn borderPrimaryBtn mr-2 ml-auto block" type="button">
+            <button className="!w-[120px] btn borderPrimaryBtn ml-auto block" type="button">
               {btnText}
             </button>
           }
@@ -13,8 +13,8 @@ const Modal = ({ btnText, children }) => {
           overlayStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
       >
           {(close) => (
-            <div className="relative mx-auto w-[80%]">
-              {children}
+            <div className={`relative mx-auto ${modalClass}`}>
+              {children(close)}
               <button onClick={close} className="absolute right-1 top-1 bg-close w-[40px] h-[40px] bg-contain" />
             </div>
           )}
